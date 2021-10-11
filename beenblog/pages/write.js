7504072c419a2c838fab41f156fff323fc8eca2a
hanxbeen <whgksqls13@naver.com>
2021-10-11 23:52:28 +0900

    feat : '게시글 입력 폼 생성'

--- beenblog/pages/write.js
+++ beenblog/pages/write.js
@@ -4,7 +4,32 @@ export default function Write() {
   return (
     <div className="container">
       <Header />
-      <h1>Write</h1>
+      {/* <h1>Write</h1> */}
+      <form className="mt-5">
+        <div className="mb-3">
+          <input
+            type="email"
+            className="form-control"
+            id="inputTitle"
+            placeholder="Title"
+          ></input>
+          <div id="titleTag" className="form-text">
+            #태그 기능 추가 예정
+          </div>
+        </div>
+        <div class="mb-3">
+          <textarea
+            type="text"
+            className="form-control"
+            id="inputContents"
+            placeholder="Contents"
+          ></textarea>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          입력
+        </button>
+      </form>
     </div>
   );
 }
