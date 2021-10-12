b1c03f6ebc0edc53840d3f2994c3ab3fca022942
Hanxbeen <hanxbeen@gmail.com>
2021-10-12 15:37:40 +0900

    feat : 'input form 수정'

--- beenblog/pages/write.js
+++ beenblog/pages/write.js
@@ -1,34 +1,59 @@
-import Header from '../components/Header';
-
+import Header from "../components/Header";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { useState } from "react";
 export default function Write() {
+  const [blogContent, setBlogContent] = useState({
+    title: "",
+    content: "",
+  });
+  const getValue = e => {
+    const { name, value } = e.target;
+      console.log(name, value);
+  };
   return (
     <div className="container">
       <Header />
-      {/* <h1>Write</h1> */}
       <form className="mt-5">
-        <div className="mb-3">
-          <input
-            type="email"
-            className="form-control"
-            id="inputTitle"
-            placeholder="Title"
-          ></input>
-          <div id="titleTag" className="form-text">
-            #태그 기능 추가 예정
-          </div>
+        <input
+          type="text"
+          className="form-control"
+          id="inputTitle"
+          placeholder="Title"
+        ></input>
+        <div id="titleTag" className="form-text my-3">
+          <span class="badge bg-primary"># Primary</span>
+          <span class="badge bg-secondary"># Secondary</span>
+          <span class="badge bg-success"># Success</span>
+          <span class="badge bg-danger"># Danger</span>
+          <span class="badge bg-warning text-dark"># Warning</span>
+          <span class="badge bg-info text-dark"># Info</span>
+          <span class="badge bg-light text-dark"># Light</span>
+          <span class="badge bg-dark"># Dark</span>
         </div>
-        <div class="mb-3">
-          <textarea
-            type="text"
-            className="form-control"
-            id="inputContents"
-            placeholder="Contents"
-          ></textarea>
+        <CKEditor
+          editor={ClassicEditor}
+          data="<p>입력하세요</p>"
+          onReady={(editor) => {
+            // You can store the "editor" and use when it is needed.
+            console.log("Editor is ready to use!", editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+          }}
+          onBlur={(event, editor) => {
+            console.log("Blur.", editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log("Focus.", editor);
+          }}
+        />
+        <div className="flex justify-center mt-3">
+          <button type="submit" className="btn btn-primary mt-3 w-24">
+            입력
+          </button>
         </div>
-
-        <button type="submit" className="btn btn-primary">
-          입력
-        </button>
       </form>
     </div>
   );
