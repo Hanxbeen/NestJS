6d34006da32f5a7cabffbd2ad8c7576c1eca4adb
Hanxbeen <hanxbeen@gmail.com>
2021-10-21 16:27:35 +0900

    feat : 'axios get REST test 성공'

--- server/index.js
+++ server/index.js
@@ -17,35 +17,45 @@ client.connect((err) => {
   if (err) {
     console.error("connection error", err.stack);
   } else {
-    console.log("success!");
+    console.log("연결 성공!");
   }
 });
 app.use(cors());
-app.post("/", function (req, res, next) {
-  const title = req.body["title"];
-  const contents = req.body["contents"];
-  const qstr = "INSERT INTO board (title, contents) values($1, $2);";
-  const query = client.query(qstr, [title, contents]);
-  console.log(title);
-  client.query(query);
-  var rows = [];
-  /**
-   * row에서 데이터 가져오고 end에서 검색할 때 발생한 각종 정보,
-   * error는 오류 발생시
-   **/
-  query.on("row", (row) => {
-    rows.push(row);
-  });
-  query.on("end", () => {
-    console.log(rows);
-    console.log("query done");
-    res.send(rows);
-    res.status(200).end();
-  });
-  query.on("error", (err) => {
-    console.error(err.stack);
-  });
+app.use(express.json());
+
+app.get("/getBoard", async (req, res) => {
+  try {
+    const getBoard = await client.query("SELECT * FROM board");
+    res.json(getBoard.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
 });
+// app.post("/", function (req, res, next) {
+//   const title = req.body["title"];
+//   const contents = req.body["contents"];
+//   const qstr = "INSERT INTO board (title, contents) values($1, $2);";
+//   const query = client.query(qstr, [title, contents]);
+//   console.log(title);
+//   client.query(query);
+//   var rows = [];
+//   /**
+//    * row에서 데이터 가져오고 end에서 검색할 때 발생한 각종 정보,
+//    * error는 오류 발생시
+//    **/
+//   query.on("row", (row) => {
+//     rows.push(row);
+//   });
+//   query.on("end", () => {
+//     console.log(rows);
+//     console.log("query done");
+//     res.send(rows);
+//     res.status(200).end();
+//   });
+//   query.on("error", (err) => {
+//     console.error(err.stack);
+//   });
+// });
 app.listen(PORT, () => {
   console.log(`running on PORT ${PORT}`);
 });
