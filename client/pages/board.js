9058153593b29dca81f80f0ab2170877c7ccf0c7
Hanxbeen <hanxbeen@gmail.com>
2021-10-27 09:07:33 +0900

    style : ' 오타 수정'

--- client/pages/board.js
+++ client/pages/board.js
@@ -63,4 +63,13 @@ export default function Board() {
               <h5 className="card-title">Card title</h5>
               <p className="card-text">
                 This is a longer card with supporting text below as a natural
-                lead-in to additio
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
