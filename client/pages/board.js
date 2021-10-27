7e4a982be32c661bc258dbfa02a0e288d030ecb4
Hanxbeen <hanxbeen@gmail.com>
2021-10-27 09:07:07 +0900

    style : ' className으로 변경'

--- client/pages/board.js
+++ client/pages/board.js
@@ -1,17 +1,17 @@
-import Header from '../components/Header';
+import Header from "../components/Header";
 
 export default function Board() {
   return (
     <div className="container">
       <Header />
       {/* <h1>Board</h1> */}
-      <div class="row row-cols-1 row-cols-md-2 g-4 mt-5">
-        <div class="col">
-          <div class="card">
-            <img src="/img/jslogo.png" class="card-img-top" alt="..."></img>
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
+      <div className="row row-cols-1 row-cols-md-2 g-4 mt-5">
+        <div className="col">
+          <div className="card">
+            <img src="/img/jslogo.png" className="card-img-top" alt="..."></img>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
                 This is a longer card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit
                 longer.
@@ -19,12 +19,16 @@ export default function Board() {
             </div>
           </div>
         </div>
-        <div class="col">
-          <div class="card">
-            <img src="/img/bootlogo.png" class="card-img-top" alt="..."></img>
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
+        <div className="col">
+          <div className="card">
+            <img
+              src="/img/bootlogo.png"
+              className="card-img-top"
+              alt="..."
+            ></img>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
                 This is a longer card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit
                 longer.
@@ -32,32 +36,31 @@ export default function Board() {
             </div>
           </div>
         </div>
-        <div class="col">
-          <div class="card">
-            <img src="/img/nextlogo.png" class="card-img-top" alt="..."></img>
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
+        <div className="col">
+          <div className="card">
+            <img
+              src="/img/nextlogo.png"
+              className="card-img-top"
+              alt="..."
+            ></img>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
                 This is a longer card with supporting text below as a natural
                 lead-in to additional content.
               </p>
             </div>
           </div>
         </div>
-        <div class="col">
-          <div class="card">
-            <img src="/img/reactlogo.png" class="card-img-top" alt="..."></img>
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
+        <div className="col">
+          <div className="card">
+            <img
+              src="/img/reactlogo.png"
+              className="card-img-top"
+              alt="..."
+            ></img>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
                 This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+                lead-in to additio
